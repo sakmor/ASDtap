6f8964e769a44b4f1c20c26e4c72148a4d849388
sakmor <sakmor@gmail.com>
2017-03-15 18:06:43 +0800

    firstAdd

---
+++ Assets/main.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class main : MonoBehaviour
+{
+    GameObject player;
+    // Use this for initialization
+    void Start()
+    {
+        player = GameObject.Find("player");
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        control();
+    }
+    void control()
+    {
+        if (Input.GetKey("a"))
+        {
+            player.GetComponent<Animator>().Play("walk");
+        }
+        if (Input.GetKey("s"))
+        {
+            player.GetComponent<Animator>().Play("walk");
+        }
+        if (Input.GetKey("d"))
+        {
+            player.GetComponent<Animator>().Play("walk");
+        }
+    }
+}
